481e854a1395a4ffb876052f4f3cccafa3ff9f9a
paulAlexSerban <paul.alex.serban@gmail.com>
2023-06-28 10:06:12 +0300

    feat(.mdz and modulize): chanegd from scripts to .mdz for modlize scripts and updated modulize

--- scripts/modulize.test.bash
+++ scripts/modulize.test.bash
@@ -1,15 +1,15 @@
-bash modulize.bash -e dev -p build -m wp-theme
-bash modulize.bash -e dev -p build -m js-component-library
-bash modulize.bash -e dev -p build -m living-style-guide
-bash modulize.bash -e dev -p clean -m wp-theme
-bash modulize.bash -e dev -p clean -m js-component-library
-bash modulize.bash -e dev -p clean -m living-style-guide
-bash modulize.bash -e dev -p uninstall
-bash modulize.bash -e dev -p install -m wp-theme
-bash modulize.bash -e dev -p install -m js-component-library
-bash modulize.bash -e dev -p install -m living-style-guide
-bash modulize.bash -e dev -p build -m traefik
-bash modulize.bash -e dev -p build -m nginx
-bash modulize.bash -e dev -p build -m database
-bash modulize.bash -e dev -p build -m wordpress
-bash modulize.bash -e dev -p build -m assets
+bash modulize -e dev -p build -m wp-theme
+bash modulize -e dev -p build -m js-component-library
+bash modulize -e dev -p build -m living-style-guide
+bash modulize -e dev -p clean -m wp-theme
+bash modulize -e dev -p clean -m js-component-library
+bash modulize -e dev -p clean -m living-style-guide
+bash modulize -e dev -p uninstall
+bash modulize -e dev -p install -m wp-theme
+bash modulize -e dev -p install -m js-component-library
+bash modulize -e dev -p install -m living-style-guide
+bash modulize -e dev -p build -m traefik
+bash modulize -e dev -p build -m nginx
+bash modulize -e dev -p build -m database
+bash modulize -e dev -p build -m wordpress
+bash modulize -e dev -p build -m assets
